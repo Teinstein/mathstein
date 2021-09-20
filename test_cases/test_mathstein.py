9dc21fa169556df9cb88a8cb111f9ee44c48d98e
GauravThakur195 <63355664+GauravThakur195@users.noreply.github.com>
2021-09-20 11:40:14 +0530

    Create test_mathstein.py

---
+++ test_cases/test_mathstein.py
@@ -0,0 +1,21 @@
+import unittest
+import mathstein
+
+
+class TestMathstein(unittest.TestCase):
+    
+    def test_OneVarSolver(self):
+        result1=mathstein.OneVarSolver("x+5-3+x=6+x-2")
+        self.assertEqual(result1,2)
+        result2=mathstein.OneVarSolver("x=x")
+        self.assertEqual(result2,"Infinite solutions")
+        result3=mathstein.OneVarSolver("x=x+2")
+        self.assertEqual(result3,"No solution")
+    def test_MultiVarSolver(self):
+        result1=mathstein.MultiVarSolver(['- 3x + y = -1','4x + y = -8'],2)
+        self.assertEqual(result1,[-1.0,-4.0])
+        result2=mathstein.MultiVarSolver(['2x + y = 2','4x + 2y = -8'],2)
+        self.assertEqual(result2,"No solution possible")
+
+if __name__=='__main__':
+    unittest.main()
