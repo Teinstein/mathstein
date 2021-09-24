024be73c0482e708c518930991530b6688f1dd5b
GauravThakur195 <63355664+GauravThakur195@users.noreply.github.com>
2021-09-25 01:18:44 +0530

    Update test_mathstein.py

--- test_cases/test_mathstein.py
+++ test_cases/test_mathstein.py
@@ -26,6 +26,11 @@ class TestMathstein(unittest.TestCase):
         self.assertEqual(result1,3)
         result2=mathstein.cubicsolver((1,0,0,27,0))
         self.assertEqual(result2,"No real root possible")
+    def test_biquadraticsolver(self):
+        result1=mathstein.biquadraticsolver((1,0,0,0,0,16))
+        self.assertEqual(result1,[-4.0, 4.0])
+        result2=mathstein.biquadraticsolver((1,0,0,0,0,-16))
+        self.assertEqual(result2,"No real root possible")
 
 if __name__=='__main__':
     unittest.main()
