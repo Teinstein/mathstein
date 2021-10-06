f3f5e9d181fb2667462f3cc01645218d6542025c
GauravThakur195 <63355664+GauravThakur195@users.noreply.github.com>
2021-10-06 14:48:10 +0530

    Update test_matheqn.py

--- test_cases/test_matheqn.py
+++ test_cases/test_matheqn.py
@@ -12,16 +12,16 @@ class TestMathstein(unittest.TestCase):
     
     def test_OneVarSolver(self):
         result1=mathstein.OneVarSolver("x+5-3+x=6+x-2")
-        self.assertEqual(result1,2)
+        self.assertAlmostEqual(result1,2,0,"OneVarSolver() failed to execute")
         result2=mathstein.OneVarSolver("x=x")
-        self.assertEqual(result2,"Infinite solutions")
+        self.assertAlmostEqual(result2,"Infinite solutions",None,"OneVarSolver() failed to execute")
         result3=mathstein.OneVarSolver("x=x+2")
-        self.assertEqual(result3,"No solution")
+        self.assertAlmostEqual(result3,"No solution",None,"OneVarSolver() failed to execute")
     def test_MultiVarSolver(self):
         result1=mathstein.MultiVarSolver(['- 3x + y = -1','4x + y = -8'],2)
-        self.assertEqual(result1,[-1.0,-4.0])
+        self.assertAlmostEqual(result1,[-1.0,-4.0],None,"MultiVarSolver() failed to execute")
         result2=mathstein.MultiVarSolver(['2x + y = 2','4x + 2y = -8'],2)
-        self.assertEqual(result2,"No solution possible")
+        self.assertAlmostEqual(result2,"No solution possible",None,"MultiVarSolver() failed to execute")
         
         
 if __name__=='__main__':
