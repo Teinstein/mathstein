1cab79ed8bb8690fe6904952312c18b1e0717277
GauravThakur195 <63355664+GauravThakur195@users.noreply.github.com>
2021-10-06 14:51:20 +0530

    Update test_mathroot.py

--- test_cases/test_mathroot.py
+++ test_cases/test_mathroot.py
@@ -12,19 +12,19 @@ class TestMathstein(unittest.TestCase):
     
     def test_quadraticsolver(self):
         result1=mathstein.quadraticsolver((1,0,-4,0))
-        self.assertEqual(result1,(2.0,-2.0))
+        self.assertAlmostEqual(result1,(2.0,-2.0),None,"quadraticsolver() failed to execute")
         result2=mathstein.quadraticsolver((1,0,4,0))
-        self.assertEqual(result2,"No real root possible")
+        self.assertAlmostEqual(result2,"No real root possible",None,"quadraticsolver() failed to execute")
     def test_cubicsolver(self):
         result1=mathstein.cubicsolver((1,0,0,-27,0))
-        self.assertEqual(result1,3)
+        self.assertAlmostEqual(result1,3,None,"cubicsolver() failed to execute")
         result2=mathstein.cubicsolver((1,0,0,27,0))
-        self.assertEqual(result2,"No real root possible")
+        self.assertAlmostEqual(result2,"No real root possible",None,"cubicsolver() failed to execute")
     def test_biquadraticsolver(self):
         result1=mathstein.biquadraticsolver((1,0,0,0,0,16))
-        self.assertEqual(result1,[-4.0, 4.0])
+        self.assertAlmostEqual(result1,[-4.0, 4.0],None,"biquadraticsolver() failed to execute")
         result2=mathstein.biquadraticsolver((1,0,0,0,0,-16))
-        self.assertEqual(result2,"No real root possible")
+        self.assertAlmostEqual(result2,"No real root possible",None,"biquadraticsolver() failed to execute")
         
 
 if __name__=='__main__':
