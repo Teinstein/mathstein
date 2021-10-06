9a7400d92db4596b6dc931fce5f40e72e755b30d
GauravThakur195 <63355664+GauravThakur195@users.noreply.github.com>
2021-10-06 14:45:27 +0530

    Update test_matharea.py

--- test_cases/test_matharea.py
+++ test_cases/test_matharea.py
@@ -12,9 +12,9 @@ class TestMathstein(unittest.TestCase):
     
     def test_areacalculator(self):
         result1=mathstein.areacalculator("x**3",[1,2])
-        self.assertEqual(result1,3.75450074999953)
+        self.assertAlmostEqual(result1,3.7545,3,"areacalculator() failed to execute")
         result2=mathstein.areacalculator("3*x**2+2",[1,2])
-        self.assertEqual(result2,9.009500499999444)
+        self.assertAlmostEqual(result2,9.009,3,"areacalculator() failed to execute")
 
 if __name__=='__main__':
     unittest.main()
