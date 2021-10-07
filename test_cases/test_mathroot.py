55449dd894fbb0d8634b2f73a6531c5891949a86
GauravThakur195 <63355664+GauravThakur195@users.noreply.github.com>
2021-10-07 22:19:09 +0530

    Update test_mathroot.py

--- test_cases/test_mathroot.py
+++ test_cases/test_mathroot.py
@@ -5,25 +5,25 @@
 
 
 import unittest
-import mathstein
+import mathroot
 
 
 class TestMathstein(unittest.TestCase):
     
     def test_quadraticsolver(self):
-        result1=mathstein.quadraticsolver((1,0,-4,0))
+        result1=mathroot.quadraticsolver((1,0,-4,0))
         self.assertAlmostEqual(result1,(2.0,-2.0),None,"quadraticsolver() failed to execute")
-        result2=mathstein.quadraticsolver((1,0,4,0))
+        result2=mathroot.quadraticsolver((1,0,4,0))
         self.assertAlmostEqual(result2,"No real root possible",None,"quadraticsolver() failed to execute")
     def test_cubicsolver(self):
-        result1=mathstein.cubicsolver((1,0,0,-27,0))
+        result1=mathroot.cubicsolver((1,0,0,-27,0))
         self.assertAlmostEqual(result1,3,None,"cubicsolver() failed to execute")
-        result2=mathstein.cubicsolver((1,0,0,27,0))
+        result2=mathroot.cubicsolver((1,0,0,27,0))
         self.assertAlmostEqual(result2,"No real root possible",None,"cubicsolver() failed to execute")
     def test_biquadraticsolver(self):
-        result1=mathstein.biquadraticsolver((1,0,0,0,0,16))
+        result1=mathroot.biquadraticsolver((1,0,0,0,0,16))
         self.assertAlmostEqual(result1,[-4.0, 4.0],None,"biquadraticsolver() failed to execute")
-        result2=mathstein.biquadraticsolver((1,0,0,0,0,-16))
+        result2=mathroot.biquadraticsolver((1,0,0,0,0,-16))
         self.assertAlmostEqual(result2,"No real root possible",None,"biquadraticsolver() failed to execute")
         
 
