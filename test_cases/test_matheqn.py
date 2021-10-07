988e4c85f8b2387d939ece6fd38e6fb08d75839a
GauravThakur195 <63355664+GauravThakur195@users.noreply.github.com>
2021-10-07 22:17:58 +0530

    Update test_matheqn.py

--- test_cases/test_matheqn.py
+++ test_cases/test_matheqn.py
@@ -5,22 +5,22 @@
 
 
 import unittest
-import mathstein
+import matheqn
 
 
 class TestMathstein(unittest.TestCase):
     
     def test_OneVarSolver(self):
-        result1=mathstein.OneVarSolver("x+5-3+x=6+x-2")
+        result1=matheqn.OneVarSolver("x+5-3+x=6+x-2")
         self.assertAlmostEqual(result1,2,0,"OneVarSolver() failed to execute")
-        result2=mathstein.OneVarSolver("x=x")
+        result2=matheqn.OneVarSolver("x=x")
         self.assertAlmostEqual(result2,"Infinite solutions",None,"OneVarSolver() failed to execute")
-        result3=mathstein.OneVarSolver("x=x+2")
+        result3=matheqn.OneVarSolver("x=x+2")
         self.assertAlmostEqual(result3,"No solution",None,"OneVarSolver() failed to execute")
     def test_MultiVarSolver(self):
-        result1=mathstein.MultiVarSolver(['- 3x + y = -1','4x + y = -8'],2)
+        result1=matheqn.MultiVarSolver(['- 3x + y = -1','4x + y = -8'],2)
         self.assertAlmostEqual(result1,[-1.0,-4.0],None,"MultiVarSolver() failed to execute")
-        result2=mathstein.MultiVarSolver(['2x + y = 2','4x + 2y = -8'],2)
+        result2=matheqn.MultiVarSolver(['2x + y = 2','4x + 2y = -8'],2)
         self.assertAlmostEqual(result2,"No solution possible",None,"MultiVarSolver() failed to execute")
         
         
