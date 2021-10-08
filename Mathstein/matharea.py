82425a90fd3974fc5d46297ca9ea6953bda467ba
GauravThakur195 <63355664+GauravThakur195@users.noreply.github.com>
2021-10-08 09:34:26 +0530

    Update matharea.py

--- Mathstein/matharea.py
+++ Mathstein/matharea.py
@@ -12,12 +12,24 @@ def f(function,x):
     return y
 
 def areadisplay(function,limits):
+ """
+  Shows a visual representation of area under a line or a curve
+        :param function: string which denotes the equation of the line or curve
+        :param limits: limits between which the area needs to be calculated
+        :return: Graphical object denoting area under a line or a curve
+ """
     x=np.linspace(-10,10,1000)
     plt.plot(x,f(function,x))
     plt.axhline(color="black")
     plt.fill_between(x,f(function,x),where=[(x>limits[0]) and (x<limits[1]) for x in x])
     
 def areacalculator(function,limits):
+ """
+  Calculates area covered by a line or a curve
+        :param function: string which denotes the equation of the line or curve
+        :param limits: limits between which the area needs to be calculated
+        :return: Area under line or curve
+ """
     x1=limits[0]
     x2=limits[1]
     N=1000
